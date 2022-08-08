ef42ec1d1742da6fc53c1128d9d8dec1e66971c5
Meyanis95 <yanis.meziane@essec.edu>
2022-08-08 14:14:33 +0200

    fix render backend

--- server/index.js
+++ server/index.js
@@ -27,9 +27,17 @@ app
   .use(cookieParser())
   .use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.get('/', function (req, res) {
-  res.render(path.resolve(__dirname, '../client/build/index.html'));
-});
+// app.get('/', function (req, res) {
+//   res.render(path.resolve(__dirname, '../client/build/index.html'));
+// });
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 app.get('/signup', async (req, res) => {
     const eth_address = req.query.data;
